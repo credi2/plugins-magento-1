3764cd89b1539d5cef735bace528b6fc67f6fbf2
Anton Sannikov <a.sannikov@limesoda.com>
2018-02-13 16:35:18 +0100

    Initial commit

---
+++ src/app/code/local/Limesoda/Cashpresso/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 13.02.18
+ * LimeSoda - Default (Template) Project
+ *
+ * Created by Anton Sannikov.
+ *
+ * @category    Lime_Soda
+ * @package     Default (Template) Project
+ * @copyright   Copyright (c) 2018 LimeSoda. (http://www.limesoda.com)
+ *
+ * @file ${FILE_NAME}
+ */ 
+class Limesoda_Cashpresso_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ src/app/code/local/Limesoda/Cashpresso/data/ls_cashpresso_setup/data-install-0.1.0.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 13.02.18
+ * LimeSoda - Default (Template) Project
+ *
+ * Created by Anton Sannikov.
+ *
+ * @category    Lime_Soda
+ * @package     Default (Template) Project
+ * @copyright   Copyright (c) 2018 LimeSoda. (http://www.limesoda.com)
+ *
+ * @file ${FILE_NAME}
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
